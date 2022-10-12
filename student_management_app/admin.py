e71e3436b1a6d785ec8bf8c78e6b002487117b63
KylymZ <zenisbekovk04@gmail.com>
2022-10-12 09:47:38 +0600

    Добавлено: Авторизация

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -3,3 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, LeaveReportStaff, FeedBackStudent, FeedBackStaffs, NotificationStudent, NotificationStaffs
 
 
+class UserModel(UserAdmin):
+    pass
+
+
+admin.site.register(CustomUser, UserModel)
+
+admin.site.register(AdminHOD)
--- student_management_app/models.py
+++ student_management_app/models.py
@@ -6,3 +6,6 @@ from django.dispatch import receiver
 
 
 
+class CustomUser(AbstractUser):
+    user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
