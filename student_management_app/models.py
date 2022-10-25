ac75c31fadff1e2bc7f36dd2c2b92c01a33425f1
KylymZ <zenisbekovk04@gmail.com>
2022-10-25 22:28:07 +0600

    Добавление Обратной свзяи с преподавателем (страница преподавателя)

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -70,3 +70,11 @@ class Students(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
 
+class FeedBackStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
